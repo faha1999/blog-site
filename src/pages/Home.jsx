018776cb0b3a923df0ffc1624a2f5991baafdc9c
Kawsar Ahmed Fahad <kafaha1999@gmail.com>
2022-08-12 02:58:02 +0600

    added function for search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BlogList } from '../components/BlogList';
 import { Header } from '../components/Header';
 import { Search } from '../components/Search';
 import { blogList } from '../data/blogData';
 
 export const Home = () => {
+  const [blogs, setBlogs] = useState(blogList);
+  const [searchKey, setSearchKey] = useState('');
+
+  // Search submit
+  const handleSearchBar = (e) => {
+    e.preventDefault();
+    handleSearchResults();
+  };
+
+  // Search for blog by category
+  const handleSearchResults = () => {
+    const allBlogs = blogList;
+    const filteredBlogs = allBlogs.filter((blog) =>
+      blog.category.toLowerCase().includes(searchKey.toLowerCase().trim()),
+    );
+    setBlogs(filteredBlogs);
+  };
+
+  // Clear search and show all blogs
+  const handleClearSearch = () => {
+    setBlogs(blogList);
+    setSearchKey('');
+  };
+
   return (
     <>
       <Header />
-      <Search />
-      <BlogList blogs={blogList} />
+      <Search
+        value={searchKey}
+        clearSearch={handleClearSearch}
+        formSubmit={handleSearchBar}
+        handleSearchKey={(e) => setSearchKey(e.target.value)}
+      />
+
+      {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />}
     </>
   );
 };
