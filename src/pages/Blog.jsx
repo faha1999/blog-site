da15b72fb452405d7a9b7f1ac31afad4e09eeff2
Kawsar Ahmed Fahad <kafaha1999@gmail.com>
2022-08-12 19:27:39 +0600

    added blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Chip } from '../components/common/Chip';
+import { EmptyList } from '../components/common/EmptyList';
+import { blogList } from '../data/blogData';
 
 export const Blog = () => {
-  return <div>Blog</div>;
+  const { id } = useParams();
+  const [blog, setBlog] = useState(null);
+
+  useEffect(() => {
+    let blog = blogList.find((blog) => blog.id === parseInt(id));
+    if (blog) {
+      setBlog(blog);
+    }
+  }, []);
+
+  return (
+    <>
+      <Link className="blog-goBack" to="/">
+        <span> &#8592;</span> <span>Go Back</span>
+      </Link>
+      {blog ? (
+        <div className="blog-wrap">
+          <header>
+            <p className="blog-date">Published {blog.createdAt}</p>
+            <h1>{blog.title}</h1>
+            <div className="blog-subCategory">
+              {blog.subCategory.map((category, i) => (
+                <div key={i}>
+                  <Chip label={category} />
+                </div>
+              ))}
+            </div>
+          </header>
+          <img src={blog.cover} alt="cover" />
+          <p className="blog-desc">{blog.description}</p>
+        </div>
+      ) : (
+        <EmptyList />
+      )}
+    </>
+  );
 };
